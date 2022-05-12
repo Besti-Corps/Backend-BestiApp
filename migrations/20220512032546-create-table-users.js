3ba010762fc98783b2dbf0c25be95472f052b701
MeNadhif <nadhif1495@gmail.com>
2022-05-12 20:46:38 +0700

    Initial Commit

---
+++ migrations/20220512032546-create-table-users.js
@@ -0,0 +1,51 @@
+"use strict";
+
+module.exports = {
+   async up(queryInterface, Sequelize) {
+      await queryInterface.createTable("users", {
+         id: {
+            type: Sequelize.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+            allowNull: false,
+         },
+         name: {
+            type: Sequelize.STRING,
+            allowNull: false,
+         },
+         profession: {
+            type: Sequelize.STRING,
+            allowNull: true,
+         },
+         avatar: {
+            type: Sequelize.STRING,
+            allowNull: true,
+         },
+         email: {
+            type: Sequelize.STRING,
+            allowNull: false,
+         },
+         password: {
+            type: Sequelize.STRING,
+            allowNull: false,
+         },
+         created_at: {
+            type: Sequelize.DATE,
+            allowNull: false,
+         },
+         updated_at: {
+            type: Sequelize.DATE,
+            allowNull: false,
+         },
+      });
+
+      await queryInterface.addConstraint("users", ["email"], {
+         type: "unique",
+         name: "UNIQUE_USERS_EMAIL",
+      });
+   },
+
+   async down(queryInterface, Sequelize) {
+      await queryInterface.dropTable("users");
+   },
+};
