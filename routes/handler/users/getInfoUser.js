b04d83a6c7566cdec3006fffae28070445002909
MeNadhif <nadhif1495@gmail.com>
2022-05-19 14:48:42 +0700

    Add feature GET User

---
+++ routes/handler/users/getInfoUser.js
@@ -0,0 +1,19 @@
+const { User } = require("../../../models");
+
+module.exports = async (req, res) => {
+   const id = req.params.id;
+   const user = await User.findByPk(id, {
+      attributes: ["id", "name", "phone", "email", "gender", "profession"],
+   });
+   if (!user) {
+      return res.status(404).json({
+         status: "error",
+         message: "User not found",
+      });
+   }
+
+   return res.json({
+      status: "success",
+      data: user,
+   });
+};
--- routes/handler/users/index.js
+++ routes/handler/users/index.js
@@ -1,9 +1,11 @@
 const register = require("./register");
 const login = require("./login");
 const update = require("./update");
+const getInfoUser = require("./getInfoUser");
 
 module.exports = {
    register,
    login,
    update,
+   getInfoUser,
 };
