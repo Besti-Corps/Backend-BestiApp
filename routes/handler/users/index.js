da2aea57eb241ab1675db9fb34c34607640f2902
MeNadhif <nadhif1495@gmail.com>
2022-05-19 15:56:38 +0700

    Add API Logout feature

--- routes/handler/users/index.js
+++ routes/handler/users/index.js
@@ -2,10 +2,12 @@ const register = require("./register");
 const login = require("./login");
 const update = require("./update");
 const getInfoUser = require("./getInfoUser");
+const logout = require("./logout");
 
 module.exports = {
    register,
    login,
    update,
    getInfoUser,
+   logout,
 };
