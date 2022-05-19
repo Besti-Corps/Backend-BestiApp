c867e37730218f28da4b9c05661a38d49fa2f22e
MeNadhif <nadhif1495@gmail.com>
2022-05-19 15:39:55 +0700

    Create GET Token feature and fix errors in Create Token function

--- routes/refreshToken.js
+++ routes/refreshToken.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const refreshTokenHandler = require("./handler/refreshTokens");
 
 router.post("/", refreshTokenHandler.create);
+router.get("/", refreshTokenHandler.getToken);
 
 module.exports = router;
